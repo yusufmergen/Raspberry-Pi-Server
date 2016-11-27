d08cbc253e730a6b30a3030cbe6dd665982d39f5
yusufmergen <yusufmergen@gmail.com>
2016-11-27 02:30:01 -0500

    db connection and style files

---
+++ db.php
@@ -0,0 +1,28 @@
+<?php
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+$username = stripcslashes($username);
+$password = stripcslashes($passowrd);
+
+$username = mysql_real_escape_string($username);
+$password = mysql_real_escape_string($password);
+
+mysql_connect("localhost", "root", "");
+mysql_select_db("Users");
+
+$result= mysql_query("SELECT * FROM Users WHERE Username= '$username' and Password = '$password'") or die("Failed to connect to the database".mysql_error());
+
+$row=mysql_fetch_array($result);
+
+if($row['username'] == $username && $row['password'] == $password){
+
+	echo "Welcome to SecuryPi ".$row['username'];
+
+}else{
+	echo "Login Failed";
+}
+
+
+?>
