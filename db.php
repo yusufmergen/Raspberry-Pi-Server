74c7a877f6abcedde0450e22e9a02482e0dc39a3
yusufmergen <yusufmergen@gmail.com>
2016-11-28 13:29:35 -0500

    more fixes

--- db.php
+++ db.php
@@ -1,31 +1,6 @@
 <?php
 
-$username = $_POST['username'];
-$password = $_POST['password'];
-
-$username = strip_tags($username);
-$password = strip_tags($password);
-
-$username = stripcslashes($username);
-$password = stripcslashes($password);
-
-$username = mysql_real_escape_string($username);
-$password = mysql_real_escape_string($password);
-
-mysql_connect("localhost", "root", "password");
+$db = mysql_connect("localhost", "root", "password");
 mysql_select_db("SecuryPi");
-
-$result= mysql_query("SELECT * FROM Users WHERE Username= '$username' and Password = '$password'") or die("Failed to connect to the database".mysql_error());
-
-$row=mysql_fetch_array($result);
-
-if($row['username'] == $username && $row['password'] == $password){
-
-	echo "Welcome to SecuryPi ".$row['username'];
-
-}else{
-	echo "Login Failed";
-}
-
 
 ?>
