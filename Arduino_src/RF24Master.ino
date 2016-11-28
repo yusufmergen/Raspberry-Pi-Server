cd0fb7b8d533011b2d12ddc8afe572c2e070f528
yusufmergen <yusufmergen@gmail.com>
2016-11-28 16:20:49 -0500

    arduino files added

--- Arduino_src/RF24Master.ino
+++ Arduino_src/RF24Master.ino
@@ -18,25 +18,69 @@ struct payload_t {                  // Structure of our payload
 
 
 
+
+
 void setup() {
   Serial.begin(9600);
   Serial.println("RF24Network Begins..");
   SPI.begin();
   radio.begin();
-  network.begin(100,master);
+  network.begin(/*channel*/ 100, /*node address*/ master);
 }
 
 void loop() {
   network.update();
+  while(network.available()){
+    int sensorData;
+    RF24NetworkHeader header;
+    
+    network.read(header,&sensorData,sizeof(sensorData));
+    uint16_t headerId = header.from_node;
+    
+    if(headerId == 1){
+      Serial.print("Alert!! Living Room ");
+      Serial.println(sensorData); 
+    }
+    if(headerId == 2){
+      Serial.print("Alert!! Kitchen Door ");
+      Serial.println(sensorData); 
+    }
+    
+    if(headerId == 3){
 
-while(network.available()){
+    /*String data = String(sensorData);
+    char buff[4];
+    buff[1] = data.charAt(0);
+    buff[2] = data.charAt(1);
+    buff[3] = data.charAt(2);
+    buff[4] = data.charAt(3);
+    char a[2] = {buff[1], buff[2]};
+    char b[2] = {buff[3], buff[4]};
+    int temperature = atoi(a);
+    int humidity = atoi(b);
+    Serial.println("Temperature:");
+    Serial.println(temperature, DEC);
+    Serial.println(humidity, DEC);*/
+    Serial.println("Temperature & Humidity:");
+    Serial.println(sensorData);
+    
+    }
+    
+    /*payload_t payload;
+    network.read(header,&payload,sizeof(payload));
+    Serial.println(payload.temperature);
+    Serial.print("Humidity:");
+    Serial.println(payload.humidity);
+    }*/
+
+/*while(network.available()){
   RF24NetworkHeader header;
   payload_t payload;
   network.read(header,&payload,sizeof(payload));
     Serial.print("Temperature:");
     Serial.println(payload.temperature);
     Serial.print("Humidity:");
-    Serial.println(payload.humidity);
+    Serial.println(payload.humidity);*/
 }
   
 
--- Arduino_src/RF24magSwitch.ino
+++ Arduino_src/RF24magSwitch.ino
@@ -1,10 +1,7 @@
 #include <RF24Network.h>
 #include <RF24.h>
 #include<SPI.h>
-#include "DHT.h"
 
-
-DHT dht;
 
 RF24 radio(7, 8);
 RF24Network network(radio);
@@ -13,42 +10,47 @@ const uint16_t master = 00;
 const uint16_t pir = 01;
 const uint16_t magSwitch = 02;
 const uint16_t temphumd = 03;
+int led = 3;
+int inputPin = 2;
 
-struct payload_t {                  // Structure of our payload
+
+/*struct payload_t {                  // Structure of our payload
   float temperature;
   float humidity;
-};
+};*/
 
 
 
 
 void setup() {
+  pinMode(led,  OUTPUT);
+  pinMode(inputPin, INPUT);
   Serial.begin(9600);
   Serial.println("RF24Network Begins..");
   SPI.begin();
   radio.begin();
-  network.begin(100,temphumd);
-  dht.setup(2);
+  network.begin(/*channel*/ 100, /*node address*/ magSwitch);
 }
 
 void loop() {
 
   network.update();
-  delay(dht.getMinimumSamplingPeriod());
-  int temp=(dht.getTemperature()*1.8)+32;
-  int humd=dht.getHumidity();
-  String t_payload ={String(temp)+String(humd)};
-  int payload = t_payload.toInt();
- 
-  
-  //payload_t payload = {dht.getTemperature(),dht.getHumidity()};
   RF24NetworkHeader header(/*to node*/ master);
-  bool ok = network.write(header, &payload, sizeof(payload));
-    if(ok)
-  Serial.println("Sending");
-    else
-  Serial.println("Failed to send");
-  delay(5000);
+  if(digitalRead(inputPin) == HIGH){
   
- 
+  int payload = 2;
+  digitalWrite(led,HIGH);
+  delay(500);
+  digitalWrite(led,LOW);
+  delay(500);
+  
+  bool ok = network.write(header, &payload, sizeof(payload));
+  if(ok)
+  Serial.println("Sending");
+  else
+  Serial.println("Failed to send");
+  
+  }
+  
+
 }
